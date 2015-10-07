bb8b37a82584f20b686ba095109c2caca4bd3030
Josh Manis <manisjp@s.dcsdk12.org>
2015-10-07 10:36:05 -0600

    Player move

--- main.rb
+++ main.rb
@@ -1,4 +1,5 @@
 require "gosu"
+require_relative 'player'
 
 class GameWindow < Gosu::Window
 
@@ -8,13 +9,20 @@ class GameWindow < Gosu::Window
 
 		@background_image = Gosu::Image.new("media/space.png",
 																				:tileable => true)
+		@player = Player.new
+		@player.warp(width/2.0, height/2.0)
 	end
 
 	def update
-			
+		@player.turn_left if Gosu::button_down? Gosu::KbLeft
+		@player.turn_right if Gosu::button_down? Gosu::KbRight
+		@player.accelerate if Gosu::button_down? Gosu::KbUp
+
+		@player.move
 	end
 
 	def draw
+		@player.draw
 		@background_image.draw(0, 0, 0)
 	end
 
--- player.rb
+++ player.rb
@@ -33,14 +33,14 @@ class Player
 		@y += @vel_y
 
 		@x %= 640
-		@x %= 480
+		@y %= 480
 
 		@vel_x *= 0.95
 		@vel_y *= 0.95
 	end
 
 	def draw
-		@image.draw_rot(@x, @y, 1, @angles)
+		@image.draw_rot(@x, @y, 1, @angle)
 	end
 
 end
